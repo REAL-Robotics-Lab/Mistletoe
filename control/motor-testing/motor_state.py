cb1a7980e887aa3ce1f6b2887d592a1eeb71befa
Brandon Stevens <brandonnms@gmail.com>
2023-08-31 18:07:20 -0700

    Motor changes

--- control/motor-testing/motor_state.py
+++ control/motor-testing/motor_state.py
@@ -3,14 +3,12 @@ import moteus
 
 class MotorState:
     position: float
-    trajectory_complete: bool
 
     def update(self, state: Dict):
-        self.position = state[moteus.Register.POSITION]
-        self.trajectory_complete = state[moteus.Register.TRAJECTORY_COMPLETE]
+        self.position = state.values[moteus.Register.POSITION]
 
     def get_angle(self):
         return self.position * 360
     
     def __repr__(self) -> str:
-        return f'Position: {self.position}\nTrajectory Complete: {self.trajectory_complete}'
+        return f'Position: {self.get_angle()}'
--- control/motor-testing/motor_test.py
+++ control/motor-testing/motor_test.py
@@ -2,6 +2,7 @@
 
 import moteus
 import asyncio
+import time
 import argparse
 
 from motor_state import MotorState
@@ -11,22 +12,27 @@ parser = argparse.ArgumentParser(
     description='Load configuration file and configure motor'
 )
 parser.add_argument('id', help='CAN ID', type=int)
+parser.add_argument('angle', help='Angle to set the motor to', type=float)
 args = parser.parse_args()
+controller: moteus.Controller = moteus.Controller(id=args.id)
+motor: MotorState = MotorState()
+transport: moteus.Transport = moteus.get_singleton_transport()
 
 async def main():
-    controller: moteus.Controller = moteus.Controller(id=args.id)
-    motor: MotorState = MotorState()
-    transport: moteus.Transport = moteus.get_singleton_transport()
-
-    command: moteus.Command = controller.make_position(position=0, query=True, maximum_torque=1, velocity=5)
-    await transport.cycle([command])
-
     while True:
-        command = controller.make_query()
+        command = controller.make_position(position=None, stop_position=args.angle / 360,query=True, maximum_torque=1, velocity=0.1,watchdog_timeout=None)
         states: [moteus.Command] = await transport.cycle([command])
 
         motor.update(states[0])
 
         print(motor)
 
-asyncio.run(main())
+        time.sleep(0.05)
+
+async def clean():
+    await transport.cycle([controller.make_stop()])
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    asyncio.run(clean())
