e70fb29a2d85721e9920a786d09074962037ccdd
Kyle Huang <kylehuang06@gmail.com>
2023-11-11 21:27:01 -0800

    pihat stuff

---
+++ control/pihat/motor_test.py
@@ -0,0 +1,39 @@
+# Test motor configuration
+
+import moteus
+import asyncio
+import time
+import argparse
+
+from motor_state import MotorState
+
+parser = argparse.ArgumentParser(
+    prog='Configure Motor',
+    description='Load configuration file and configure motor'
+)
+parser.add_argument('id', help='CAN ID', type=int)
+parser.add_argument('angle', help='Angle to set the motor to', type=float)
+args = parser.parse_args()
+
+controller: moteus.Controller = moteus.Controller(id=args.id)
+motor: MotorState = MotorState()
+transport: moteus.Transport = moteus.get_singleton_transport()
+
+async def main():
+    while True:
+        command = controller.make_position(position=0.5, query=True, maximum_torque=1.0, velocity=0.0, velocity_limit=0.5, accel_limit=2.0)
+        states: [moteus.Command] = await transport.cycle([command])
+
+        motor.update(states[0])
+
+        print(motor)
+
+        time.sleep(0.05)
+
+async def clean():
+    await transport.cycle([controller.make_stop()])
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    asyncio.run(clean())
---
+++ control/pihat/util.py
@@ -0,0 +1,32 @@
+import moteus_pi3hat
+
+def generate_transport():
+    can_cfg = moteus_pi3hat.CanConfiguration()
+    can_cfg.slow_bitrate = 1_000_000
+    can_cfg.fast_bitrate = 5_000_000
+    can_cfg.fdcan_frame = False
+    can_cfg.bitrate_switch = False
+    can_cfg.automatic_retransmission = True
+    can_cfg.restricted_mode = False
+    can_cfg.bus_monitor = False
+
+    # If buses are not listed, then they default to the parameters
+    # necessary to communicate with a moteus controller.
+    can_config = {
+        1: can_cfg,
+        2: can_cfg,
+        3: can_cfg,
+        4: can_cfg
+    }
+
+    # Define servo to bus map
+    servo_bus_map = {
+        1 : [11, 12], 
+        2 : [21, 22],
+        3 : [31, 32],
+        4 : [41, 42]
+    }
+
+    # Command Transport
+    return moteus_pi3hat.Pi3HatRouter(can=can_config,
+                                      servo_bus_map=servo_bus_map)
