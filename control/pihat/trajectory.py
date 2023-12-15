47a07379b6ef9fb4a6fbcde8dade7b0a0ece084e
Kyle Huang <kylehuang06@gmail.com>
2023-12-14 17:59:43 -0800

    Modify trajectories

--- control/pihat/trajectory.py
+++ control/pihat/trajectory.py
@@ -3,18 +3,22 @@ import matplotlib.pyplot as plt
 import math
 import tinyik
 import util
+
+
 class Trajectory(ABC):
     @abstractmethod
     def __init__(self) -> None:
         pass
 
     @abstractmethod
-    def get_next_state(self) -> tuple[tuple[float, float], tuple[float,float]]:
+    def get_next_state(self) -> tuple[tuple[float, float], tuple[float, float]]:
         pass
+
 
 class PredeterminedTrajectory(Trajectory):
     finished: bool
     counter: int
+    length: int
 
     leg_center_distance: float
 
@@ -26,30 +30,45 @@ class PredeterminedTrajectory(Trajectory):
         self.leg_center_distance = leg_center_distance
         self.finished = False
         self.angles, self.velocities = self.generate_trajectory()
+        self.length = len(self.angles)
 
     def get_finished(self) -> bool:
         return self.finished
 
-    def get_next_state(self) -> tuple[tuple[float, float], tuple[float,float]]:
-        if self.counter >= len(self.angles) -1:
+    def get_next_state(self) -> tuple[tuple[float, float], tuple[float, float]]:
+        if self.counter >= self.length - 1:
             self.finished = True
-            
+
         if self.finished == False:
             self.counter += 1
         else:
             self.finished = False
             self.counter = 0
-        
+
         # convert to revs
-        angle = (util.radians_to_revs((self.angles[self.counter][0])), util.radians_to_revs((self.angles[self.counter][1])))
+        angle = (
+            util.radians_to_revs((self.angles[self.counter][0])),
+            util.radians_to_revs((self.angles[self.counter][1])),
+        )
         velocity = self.velocities[self.counter]
         print(angle)
-        
+
         return angle, velocity
-    
+
     @abstractmethod
     def generate_trajectory(self) -> tuple[list[tuple], list[tuple]]:
         pass
+
+    def set_counter(self, pos: int):
+        if (pos >= self.length):
+            pos = self.length - 1
+        self.counter = int(pos)
+
+    def set_counter_percent(self, percent: float):
+        """
+        Set the counter to a percent from 0 to 1.
+        """
+        self.set_counter(int((self.length - 1) * percent))
 
     def plot(self):
         zeroes = []
@@ -57,7 +76,7 @@ class PredeterminedTrajectory(Trajectory):
         joint1_y = []
         joint2_x = []
         joint2_y = []
-        
+
         for angle in self.angles:
             joint1_x.append(self.leg_center_distance * math.cos(angle[0]))
             joint1_y.append(self.leg_center_distance * math.sin(angle[0]))
@@ -65,34 +84,62 @@ class PredeterminedTrajectory(Trajectory):
             joint2_y.append(self.leg_center_distance * math.sin(angle[1] + angle[0]))
             zeroes.append(0)
 
-        plt.quiver(zeroes, zeroes, joint1_x, joint1_y, color='b', angles='xy', scale_units='xy', scale=1)
-        plt.quiver(joint1_x, joint1_y, joint2_x, joint2_y, color='b', angles='xy', scale_units='xy', scale=1)
+        plt.quiver(
+            zeroes,
+            zeroes,
+            joint1_x,
+            joint1_y,
+            color="b",
+            angles="xy",
+            scale_units="xy",
+            scale=1,
+        )
+        plt.quiver(
+            joint1_x,
+            joint1_y,
+            joint2_x,
+            joint2_y,
+            color="b",
+            angles="xy",
+            scale_units="xy",
+            scale=1,
+        )
 
-        # TODO: prob should change limits to generalize to any trajectory 
+        # TODO: prob should change limits to generalize to any trajectory
 
-        plt.xlim(-0.15, 0.2) 
-        plt.ylim(-0.3, 0.05) 
+        plt.xlim(-0.15, 0.2)
+        plt.ylim(-0.3, 0.05)
         plt.show()
+
 
 class HalfCircleTrajectory(PredeterminedTrajectory):
     num_setpoints: int
 
     num_setpoints_swing: int
     num_setpoints_drag: int
-    
-    dist_to_ground : float
+
+    dist_to_ground: float
     swing_radius: float
 
     leg_ik: tinyik.Actuator
 
-    def __init__(self, num_setpoints, leg_center_distance, dist_to_ground, swing_radius, uniform_velocity) -> None:
+    def __init__(
+        self,
+        num_setpoints,
+        leg_center_distance,
+        dist_to_ground,
+        swing_radius,
+        uniform_velocity,
+    ) -> None:
         self.num_setpoints = num_setpoints
 
-        self.num_setpoints_swing = int((math.pi / (2 + math.pi)) * num_setpoints) 
+        self.num_setpoints_swing = int((math.pi / (2 + math.pi)) * num_setpoints)
         self.num_setpoints_drag = int((2 / (2 + math.pi)) * num_setpoints)
-        
-        self.leg_ik = tinyik.Actuator(['z', [leg_center_distance, 0., 0.], 'z', [leg_center_distance, 0., 0.]])
-        
+
+        self.leg_ik = tinyik.Actuator(
+            ["z", [leg_center_distance, 0.0, 0.0], "z", [leg_center_distance, 0.0, 0.0]]
+        )
+
         self.dist_to_ground = dist_to_ground
         self.swing_radius = swing_radius
 
@@ -100,64 +147,103 @@ class HalfCircleTrajectory(PredeterminedTrajectory):
 
         super().__init__(leg_center_distance)
 
-
     def generate_trajectory(self) -> tuple[list[tuple], list[tuple]]:
-
         trajectory_pos_x = 0
-        trajectory_pos_y = self.dist_to_ground # init @ dist to ground
+        trajectory_pos_y = self.dist_to_ground  # init @ dist to ground
 
         self.leg_ik.ee = [trajectory_pos_x, trajectory_pos_y, 0]
-        
-        angle_per_step = math.pi/self.num_setpoints_swing
-        theta = 0 
+
+        angle_per_step = math.pi / self.num_setpoints_swing
+        theta = 0
 
         angles = []
         velocities = []
 
-        for step_counter in range(self.num_setpoints):      
-            if step_counter < self.num_setpoints_drag/2 or step_counter > self.num_setpoints_drag/2 + self.num_setpoints_swing: 
+        for step_counter in range(self.num_setpoints):
+            if (
+                step_counter < self.num_setpoints_drag / 2
+                or step_counter > self.num_setpoints_drag / 2 + self.num_setpoints_swing
+            ):
                 self.leg_ik.ee = [trajectory_pos_x, trajectory_pos_y, 0]
-                
+
                 angles.append((self.leg_ik.angles[0], self.leg_ik.angles[1]))
                 velocities.append((self.uniform_velocity, self.uniform_velocity))
-                
-                trajectory_pos_x += self.swing_radius/(self.num_setpoints_drag/2)
+
+                trajectory_pos_x += self.swing_radius / (self.num_setpoints_drag / 2)
                 trajectory_pos_y = self.dist_to_ground
             else:
                 theta += angle_per_step
-                trajectory_pos_y = self.dist_to_ground + self.swing_radius * math.sin(theta)
+                trajectory_pos_y = self.dist_to_ground + self.swing_radius * math.sin(
+                    theta
+                )
                 # print('theta: ' + str(theta))
                 # print('sin * rad: ' + str(self.swing_radius * math.sin(theta)))
                 # print('should be 0: ' + str(math.sin(theta)))
                 trajectory_pos_x = self.swing_radius * math.cos(theta)
                 self.leg_ik.ee = [trajectory_pos_x, trajectory_pos_y, 0]
-                
+
                 angles.append((self.leg_ik.angles[0], self.leg_ik.angles[1]))
                 velocities.append((self.uniform_velocity, self.uniform_velocity))
-                
 
             # print(theta)
             # print(self.swing_radius * math.sin(theta))
 
         return angles, velocities
 
-class StandingTrajectory(PredeterminedTrajectory):
 
+class StandingTrajectory(PredeterminedTrajectory):
     dist_to_ground: float
     leg_ik: tinyik.Actuator
 
     def __init__(self, leg_center_distance, dist_to_ground) -> None:
         self.dist_to_ground = dist_to_ground
-        self.leg_ik = tinyik.Actuator(['z', [leg_center_distance, 0.05, 0.], 'z', [leg_center_distance, 0.05, 0.]])
+        self.leg_ik = tinyik.Actuator(
+            [
+                "z",
+                [leg_center_distance, 0.05, 0.0],
+                "z",
+                [leg_center_distance, 0.05, 0.0],
+            ]
+        )
         super().__init__(leg_center_distance)
-    
+
     def generate_trajectory(self) -> tuple[list[tuple], list[tuple]]:
         self.leg_ik.ee = [0, self.dist_to_ground, 0]
-        velocity = (0,0)
+        velocity = (0, 0)
         angle = (self.leg_ik.angles[0], self.leg_ik.angles[1])
         print(angle)
         return [angle], [velocity]
-    
+
+
+class GetUpTrajectory(PredeterminedTrajectory):
+    num_setpoints: int
+    dist_to_ground: float
+    uniform_velocity: float
+    leg_ik: tinyik.Actuator
+
+    def __init__(self, num_setpoints: int, dist_to_ground: float, uniform_velocity: float):
+        self.num_setpoints = num_setpoints
+        self.dist_to_ground = dist_to_ground
+        self.uniform_velocity = uniform_velocity
+
+    def generate_trajectory(self) -> tuple[list[tuple], list[tuple]]:
+        trajectory_pos_x = 0
+        # init a little below the rotational point
+        trajectory_pos_y = self.dist_to_ground / 10
+
+        self.leg_ik.ee = [trajectory_pos_x, trajectory_pos_y, 0]
+
+        remaining_length = self.dist_to_ground * 9 / 10
+        y_increment = remaining_length / self.num_setpoints
+
+        angles = []
+
+        for step in range(self.num_setpoints):
+            trajectory_pos_y += y_increment
+            angles.append(self.leg_ik.angles[0], self.leg_ik.angles[1])
+        
+        return angles, [(self.uniform_velocity, self.uniform_velocity) for i in range(self.num_setpoints)]
+
 if __name__ == "__main__":
     leg_center_dist_mm = 175.87
     leg_center_dist_m = leg_center_dist_mm / 1000
